c1126b81a3e95f13aab9ff33d0a1189deba68eb4
Wojciech Murlikiewicz <250984@edu.p.lodz.pl>
2024-05-09 22:30:46 +0200

    Menu "kinda works". Absolute basics. Old screens don't dissapear when new ones show up.

--- CMDPlatformerCPP/CMDPlatformerCPP.cpp
+++ CMDPlatformerCPP/CMDPlatformerCPP.cpp
@@ -1,8 +1,47 @@
 ﻿#include <iostream>
 using namespace std;
 
+int exitGame = 0;
+
+int menu(bool firstTime)
+{
+	
+	cout << "****Game!****" << endl;
+	cout << "1. Play Game" << endl; //Not working
+	cout << "2. Scoreboard" << endl; //Not working
+	cout << "3. Exit" << endl;
+	cout << "Please enter the number of the option you would like to select: ";
+	int choice;
+	cin >> choice;
+
+	return choice;
+}
+void choice(int choice)
+{
+	switch (choice)
+	{
+	case 1:
+		cout << "Play Game" << endl;
+		break;
+	case 2:
+		cout << "Scoreboard" << endl;
+		break;
+	case 3:
+		exitGame = 1;
+		break;
+	default:
+		break;
+	}
+}
 
 int main()
 {
-    cout << "Hello World!\n";
+	int pick = menu(true);
+	choice(pick);
+	while (exitGame == 0)
+	{
+		pick = menu(false);
+		choice(pick);
+	}
+	return 0;
 }
