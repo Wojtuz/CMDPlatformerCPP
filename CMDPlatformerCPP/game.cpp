83298556f4d7f33837bd831b13122707604e9461
Wojciech Murlikiewicz <250984@edu.p.lodz.pl>
2024-05-10 17:49:55 +0200

    Added map generation and basic movement (to be changed, it's more snake-like than mario-like)

--- CMDPlatformerCPP/game.cpp
+++ CMDPlatformerCPP/game.cpp
@@ -1,12 +1,65 @@
 #include "header.h"
 #include <Windows.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+char map[60][20];
+int position[2] = { 0, 0 };
+
+
+void updatePosition()
+{
+	if (GetAsyncKeyState(VK_LEFT) && position[1] > 0)
+	{
+		position[1]--;
+	}
+	if (GetAsyncKeyState(VK_RIGHT) && position[1] < 19)
+	{
+		position[1]++;
+	}
+	if (GetAsyncKeyState(VK_UP) && position[0] > 0)
+	{
+		position[0]--;
+	}
+	if (GetAsyncKeyState(VK_DOWN) && position[0] < 19)
+	{
+		position[0]++;
+	}
+}
+
+void drawMap()
+{
+	fstream file;
+	file.open("map.txt", ios::in);
+	for (int i = 0; i < 60; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+			map[i][j] = ' ';
+		}
+	}
+	file.close();
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+			if (i == position[0] && j == position[1])
+			{
+				cout << "O";
+			}
+			else
+			{
+				cout << map[i][j];
+			}
+		}
+		cout << endl;
+	}
+}
 
 void playGame()
 {
 	int life = 3;
-	int position[] = { 0, 0 };
 	int score = 0;
 	bool isOnGround = true;
 	bool isJumping = false;
@@ -14,24 +67,22 @@ void playGame()
 	bool isMovingLeft = false;
 	bool isMovingRight = false;
 	bool isDead = false;
+
+
+	for (int i = 0; i < 60; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+			map[i][j] = ' ';
+		}
+	}
+
 	while (life > 0)
 	{
-		if (GetAsyncKeyState(VK_UP) & 0x8000) //Checks if the up arrow key is pressed
-		{
-			cout << "jump";
-		}
-		if (GetAsyncKeyState(VK_LEFT) & 0x8000) //Checks if the left arrow key is pressed
-		{
-			cout << "left";
-		}
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) //Checks if the right arrow key is pressed
-		{
-			cout << "right";
-		}
-		if (GetAsyncKeyState(VK_DOWN) & 0x8000) //Checks if the down arrow key is pressed
-		{
-			cout << "duck";
-		}
-		Sleep(10); //This is the delay between each frame (miliseconds)
+		cout << "Lives: " << life << "                      Score: " << score << endl;
+		drawMap();
+		updatePosition();
+		Sleep(15); //This is the delay between each frame (miliseconds)
+		system("cls"); //This is the command to clear the console
 	}
 }
